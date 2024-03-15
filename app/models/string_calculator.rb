b419ac6c924a77751c4f2f9236fedd61de43f261
Mohammad Shahnawaz <mohammadshahnawazngp7@gmail.com>
2024-03-15 14:41:10 +0530

    add functionality to Support different delimiters

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,6 +1,14 @@
 class StringCalculator < ApplicationRecord
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.gsub("\n", ',').split(',').map(&:to_i).sum
+    delimiter = ','
+    if numbers.start_with?("//")
+      delimiter_line, numbers = numbers.split("\n", 2)
+      delimiter = delimiter_line[2..-1]
+    end
+
+    numbers.gsub!("\n", delimiter)
+
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -19,5 +19,11 @@ RSpec.describe StringCalculator, type: :model do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when given a custom delimiter' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
