b4146ccafb18a4d9fef341dfef42720fcfea1b03
Mohammad Shahnawaz <mohammadshahnawazngp7@gmail.com>
2024-03-15 14:22:59 +0530

    add functionality to handle numbers separated by new lines and commas

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator < ApplicationRecord
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.split(',').map(&:to_i).sum
+    numbers.gsub("\n", ',').split(',').map(&:to_i).sum
   end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe StringCalculator, type: :model do
         expect(StringCalculator.add('5')).to eq(5)
       end
     end
+
+    context 'when given numbers with new lines between them' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
   end
 end
